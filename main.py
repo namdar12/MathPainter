6bebe56fa9c54dcbd165a64cfe8a5378a3b61d13
namdar12 <namdar.mesri12@gmail.com>
2021-10-31 14:54:56 -0700

    Created a class/ Method for each shape so that user can add squares and rectangles to canvas

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import numpy as np
+from PIL import Image
 
-
-class Cavnas:
+class Canvas:
 
     def __init__(self, width, height, color):
         self.width = width
         self.height = height
         self.color = color
+        self.data = np.zeros((self.width, self.height, 3), dtype=np.uint8)
+        self.data[:] = self.color
 
-    def make(self):
-        data = np.zeros((self.width, self.height, 3), dtype=np.unit8)  # Create a 3d numpy array of zeros
-        data[:] = self.color
+
+    def make(self):  # Create a 3d numpy array of zeros
+        img = Image.fromarray(self.data, 'RGB')
+        img.save("canvas.png")
 
 
 class Square:
@@ -21,11 +24,8 @@ class Square:
         self.size_of_side = size_of_side
         self.color = color
 
-    def draw(self):
-        square = np.zeros((self.size_of_side,self.size_of_side,3))
-        square[self.point0:self.point1+1] = self.color
-
-
+    def draw(self,canvas):
+        canvas.data[self.point0:self.point0+self.size_of_side,self.point1:self.point1+self.size_of_side] = self.color
 
 
 class Rectangle:
@@ -37,12 +37,15 @@ class Rectangle:
         self.height = height
         self.color = color
 
-    def draw(self):
-        rectangle = np.zeros((self.width, self.height, 3))
-        rectangle[self.point0:self.point1 + 1] = self.color
+    def draw(self,canvas):
+        canvas.data[self.point0:self.point0 + self.width,self.point1:self.point1 + self.height] = self.color
 
-
-if __import__(name="main.py"):
-
+if __name__ == "__main__":
+    canvas = Canvas(1000,1000,[100,100,100])
+    square = Square(0,5,100,[105,110,115])
+    square.draw(canvas)
+    rectangle = Rectangle(110,115,100,200,[125,130,125])
+    rectangle.draw(canvas)
+    canvas.make()
 
 # Challenge create a circle shape class
